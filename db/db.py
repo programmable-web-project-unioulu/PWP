937d19aaac8845a62b0634cd1a7bbb0232735400
Svenskapojkarna <hytonenaleksi@gmail.com>
2020-02-19 16:34:30 +0200

    Changed database model

--- db/db.py
+++ db/db.py
@@ -6,74 +6,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-class January(db.Model):
-    date = db.Column(db.Integer, primary_key=True)
-    link = db.Column(db.String(256), nullable=True)
-    headline = db.Column(db.String(128), nullable=False)
-    modtime = db.Column(db.DateTime, nullable=False)
-	
-class February(db.Model):
-    date = db.Column(db.Integer, primary_key=True)
-    link = db.Column(db.String(256), nullable=True)
-    headline = db.Column(db.String(128), nullable=False)
-    modtime = db.Column(db.DateTime, nullable=False)
-
-class March(db.Model):
-    date = db.Column(db.Integer, primary_key=True)
-    link = db.Column(db.String(256), nullable=True)
-    headline = db.Column(db.String(128), nullable=False)
-    modtime = db.Column(db.DateTime, nullable=False)
-
-class April(db.Model):
-    date = db.Column(db.Integer, primary_key=True)
-    link = db.Column(db.String(256), nullable=True)
-    headline = db.Column(db.String(128), nullable=False)
-    modtime = db.Column(db.DateTime, nullable=False)
-
-class May(db.Model):
-    date = db.Column(db.Integer, primary_key=True)
-    link = db.Column(db.String(256), nullable=True)
-    headline = db.Column(db.String(128), nullable=False)
-    modtime = db.Column(db.DateTime, nullable=False)
-
-class June(db.Model):
-    date = db.Column(db.Integer, primary_key=True)
-    link = db.Column(db.String(256), nullable=True)
-    headline = db.Column(db.String(128), nullable=False)
-    modtime = db.Column(db.DateTime, nullable=False)
-
-class July(db.Model):
-    date = db.Column(db.Integer, primary_key=True)
-    link = db.Column(db.String(256), nullable=True)
-    headline = db.Column(db.String(128), nullable=False)
-    modtime = db.Column(db.DateTime, nullable=False)
-
-class August(db.Model):
-    date = db.Column(db.Integer, primary_key=True)
-    link = db.Column(db.String(256), nullable=True)
-    headline = db.Column(db.String(128), nullable=False)
-    modtime = db.Column(db.DateTime, nullable=False)
-
-class September(db.Model):
-    date = db.Column(db.Integer, primary_key=True)
-    link = db.Column(db.String(256), nullable=True)
-    headline = db.Column(db.String(128), nullable=False)
-    modtime = db.Column(db.DateTime, nullable=False)
-
-class October(db.Model):
-    date = db.Column(db.Integer, primary_key=True)
-    link = db.Column(db.String(256), nullable=True)
-    headline = db.Column(db.String(128), nullable=False)
-    modtime = db.Column(db.DateTime, nullable=False)
-
-class November(db.Model):
-    date = db.Column(db.Integer, primary_key=True)
-    link = db.Column(db.String(256), nullable=True)
-    headline = db.Column(db.String(128), nullable=False)
-    modtime = db.Column(db.DateTime, nullable=False)
-
-class December(db.Model):
-    date = db.Column(db.Integer, primary_key=True)
+class Articles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(32), unique=True, nullable=False)
     link = db.Column(db.String(256), nullable=True)
     headline = db.Column(db.String(128), nullable=False)
     modtime = db.Column(db.DateTime, nullable=False)
@@ -88,6 +23,5 @@ class AddedArticle(db.Model):
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.Integer, unique=True, nullable=False)
-    name = db.Column(db.String(32), nullable=False)
+    username = db.Column(db.String(32), unique=True, nullable=False)
     article = db.relationship("AddedArticle", back_populates="owner")
