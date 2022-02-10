3e42f53cf21c665f6f377556de8de144d141bc3d
Jonah Siedler <jonah.siedler@gmail.com>
2022-02-11 00:24:35 +0200

    add the database implementation with test data

---
+++ backend/database/database.py
@@ -0,0 +1,64 @@
+import enum
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movie-review.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+	cursor = dbapi_connection.cursor()
+	cursor.execute("PRAGMA foreign_keys=ON")
+	cursor.close()
+
+
+class Movie(db.Model):
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	title = db.Column(db.String, nullable=False)
+	director = db.Column(db.String, nullable=False)
+	length = db.Column(db.Integer, nullable=False)
+	release_date = db.Column(db.Date, nullable=False)
+	category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="RESTRICT"), nullable=False)
+
+	category = db.relationship("Category")
+	reviews = db.relationship("Review", back_populates="movie")
+
+
+class Category(db.Model):
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	title = db.Column(db.String, nullable=False)
+
+	movie = db.relationship("Movie", back_populates="category")
+
+
+class Review(db.Model):
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	rating = db.Column(db.Integer, nullable=False)
+	comment = db.Column(db.Text, nullable=False)
+	date = db.Column(db.Date, nullable=False)
+	author_id = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="SET NULL"), nullable=True)
+	movie_id = db.Column(db.Integer, db.ForeignKey("movie.id", ondelete="CASCADE"), nullable=False)
+
+	movie = db.relationship("Movie")
+	user = db.relationship("User")
+
+
+class UserType(enum.Enum):
+	admin = "Admin"
+	basicUser = "Basic User"
+
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	username = db.Column(db.String, nullable=False, unique=True)
+	emailAddress = db.Column(db.String, nullable=False, unique=True)
+	password = db.Column(db.String, nullable=False)
+	role = db.Column(db.Enum(UserType), nullable=False)
+
+	review = db.relationship("Review", back_populates="user")
