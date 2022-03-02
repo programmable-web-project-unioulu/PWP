32a53b5de91c2fd04e536377b6a8690e31388e78
Jonah Siedler <jonah.siedler@gmail.com>
2022-03-02 18:13:32 +0200

    remove the class wrapper from the serializing methods

--- backend/helper/serializer.py
+++ backend/helper/serializer.py
@@ -1,7 +1,9 @@
-class Serializer:
-    def serialize(self):
-        return {c: getattr(self, c) for c in inspect(self).attrs.keys()}
+from sqlalchemy import inspect
 
-    @staticmethod
-    def serialize_list(l):
-        return [m.serialize() for m in l]
+
+def serialize(self):
+    return {c: getattr(self, c) for c in inspect(self).attrs.keys()}
+
+
+def serialize_list(l):
+    return [m.serialize() for m in l]
