7c4fc32d75af810bc52c889dee64b0e4f0f64bc6
Mikkeep <mikke.pennanen@gmail.com>
2022-03-18 14:50:45 +0200

    Added DIFFICULTIES constant, containing all the valid difficulties of recipes

--- Cookbook/cookbook/constants.py
+++ Cookbook/cookbook/constants.py
@@ -3,5 +3,5 @@ LINK_RELATIONS_URL = "/cookbook/link-relations/"
 ERROR_PROFILE = "/profiles/error-profile/"
 USER_PROFILE = "/profiles/user/"
 JSON = "application/json"
-
+DIFFICULTIES = ["easy", "medium", "hard"]
 MEASUREMENT_PAGE_SIZE = 50
