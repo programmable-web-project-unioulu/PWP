7dbd8ea89dd321bdec388789432299c832cac46c
Arto Kinos <akinos@student.oulu.fi>
2022-06-17 09:47:54 +0300

    Combined code from Mika's branch and added comments

--- inventory_engineer/app.py
+++ inventory_engineer/app.py
@@ -4,21 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.engine import Engine
 from sqlalchemy import event
 from django.db import IntegrityError
+from werkzeug.exceptions import HTTPException
 from sqlalchemy.ext.declarative import DeclarativeMeta
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-
-class Measurement(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    sensor_id = db.Column(db.Integer, db.ForeignKey("sensor.id"))
-    value = db.Column(db.Float, nullable=False)
-    time = db.Column(db.DateTime, nullable=False)
-
-    sensor = db.relationship("Sensor", back_populates="measurements")
-
 
 class StorageItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -28,7 +20,6 @@ class StorageItem(db.Model):
 
     product = db.relationship("Product", back_populates="in_storage")
 
-
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     handle = db.Column(db.String(64), unique=True, nullable=False)
@@ -37,19 +28,9 @@ class Product(db.Model):
 
     in_storage = db.relationship("StorageItem", back_populates="product")
 
-
-class Sensor(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32), nullable=False, unique=True)
-    model = db.Column(db.String(128), nullable=False)
-    location = db.Column(db.String(128), nullable=False)
-    latitude = db.Column(db.Float, nullable=True)
-    longitude = db.Column(db.Float, nullable=True)
-
-    measurements = db.relationship("Measurement", cascade="all, delete-orphan", back_populates="sensor")
-
 class AlchemyEncoder(json.JSONEncoder):
-    """ this class and function copied from https://stackoverflow.com/questions/5022066/how-to-serialize-sqlalchemy-result-to-json"""
+    """ this class and function copied from https://stackoverflow.com/questions/5022066/how-to-serialize-sqlalchemy-result-to-json
+        Is this in vain??? Can serialization be done differently"""
 
     def default(self, obj):
         if isinstance(obj.__class__, DeclarativeMeta):
@@ -69,6 +50,7 @@ class AlchemyEncoder(json.JSONEncoder):
 
 @app.route("/product/add/", methods=['POST'])
 def add_product():
+    """ Add Product via POST method"""
     try:
         prod_handle = request.json["handle"]
         product = Product.query.filter_by(handle=prod_handle).first()
@@ -91,20 +73,22 @@ def add_product():
 
 @app.route("/storage/<product>/add/", methods=['POST'])
 def add_to_storage(product):
+    """ Adds product to the storage - creates link between Product and Storage item"""
     try:
         product = Product.query.filter_by(handle=product).first()
         if product:
             location = str(request.json["location"])
             qty = int(request.json["qty"])
-            inventory = StorageItem(
+            storage_item = StorageItem(
                 location=location,
                 qty=qty,
                 product_id=product.id
             )
+            """ Some relationship forcing here? """
             #product = db.relationship("Product",backref=) #TODO
-            product.in_storage.append(inventory)
+            product.in_storage.append(storage_item)
             db.session.add(product)
-            db.session.add(inventory)
+            db.session.add(storage_item)
             db.session.commit()
             return "PASS",201
         else:
@@ -114,31 +98,48 @@ def add_to_storage(product):
 
 @app.route("/storage/", methods=['GET'])
 def get_inventory():
+    """ Creates JSON format output from storaged items -
+        Does this require AlchemyEncoder class or not? What is the best way to print data out in JSON format"""
     try:
         storage = Product.query.all()
         for product in storage:
-            product_as_json = json.dumps(product,cls=AlchemyEncoder)
+            #product_as_json = json.dumps(product,cls=AlchemyEncoder)
             #print(product_as_json)
-            #print(product.handle)
-            #print(product.weight)
-            #print(product.price)
+            print(product.handle)
+            print(product.weight)
+            print(product.price)
             product_id = product.id
             inventory = StorageItem.query.all()
             for item in inventory:
                 #print(item.product_id)
                 if item.product_id == product_id:
-                    #print(f"location:{item.location}")
-                    #print(f"qty: {item.qty}")
+                    print(f"location:{item.location}")
+                    print(f"qty: {item.qty}")
         return "PASS",201
     except (KeyError,ValueError, IntegrityError):
         return "Qty must be and integer", 400
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    if e.code == 405:
+        response = "POST method required", e.code
+    if e.code == 415:
+        response = "Request content must be JSON", e.code
+    return response
 
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor = dbapi_connection.cursor()
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
-
-
 
 db.create_all()
