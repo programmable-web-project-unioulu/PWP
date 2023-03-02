331428123501aee4c82715ffc8667a28738724f5
talha-zshan <talhazshan@gmail.com>
2023-03-02 13:55:22 +0200

    requirements.txt

--- logbot/models/document.py
+++ logbot/models/document.py
@@ -1,4 +1,4 @@
-from mongoengine import ( Document, StringField, DateTimeField,)
+from mongoengine import ( Document, StringField, DateTimeField, FileField)
 
 
 class User_Document(Document):
@@ -7,14 +7,14 @@ class User_Document(Document):
     Password is automatically hashed before saving.
     :param user_id: unique required login value
     :param documentId: required unique document id
-    :param document_content: option unique string username
+    :param document: option unique string username
     :param document_summary: string representation of documents summary
     :param timestamp: timestamp of entry
     """
     
     user_id = StringField(required=True, unique=True)
     document_id = StringField(required=True, unique=True)
-    document_content = StringField(max_length=6000)
+    document = FileField()
     document_summary = StringField(max_length=6000)
     timestamp = DateTimeField()
     
