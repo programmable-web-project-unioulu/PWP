6be741ed49c446aa7bb091eef436c04ac0e02376
PrasasthyKB <prasasthy.sanal@gmail.com>
2023-03-06 21:43:32 +0200

    Stage 2 API update

--- logbot/routes.py
+++ logbot/routes.py
@@ -9,6 +9,9 @@ from resources.generatetag import GenTag
 from resources.deletedocall import DelDoc
 from resources.deletesum import DelSumm
 from resources.deletetag import DelTag
+from resources.reloaddocchat import ReloadDoc
+from resources.updatesum import UpdateSum
+from resources.updatetag import UpdateTag
 
 
 def create_routes(api: Api):
@@ -30,4 +33,7 @@ def create_routes(api: Api):
     api.add_resource(DelDoc, '/deletedocall/DelDoc/')
     api.add_resource(DelSumm, '/deletesum/DelSumm/')
     api.add_resource(DelTag, '/deletetag/DelTag/')
-
+    api.add_resource(ReloadDoc, '/reloaddocchat/ReloadDoc/')
+    api.add_resource(UpdateSum, '/updatesum/UpdateSum/')
+    api.add_resource(UpdateTag, '/updatetag/UpdateTag/')
+    
