82c91f50d9ebd55f2c24ff7eec12625900191929
Moiz-Naeem <moiznaeem20@gmail.com>
2024-01-14 00:03:03 +0200

    new changes

--- Put/first.java
+++ Put/first.java
@@ -1,11 +1,12 @@
-import java.util.Scanner;
+import java.util.
 
 public class first {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Please enter your last name and first name separated by spaces: ");
+        System.out.println("Please enter your last name and first name: ");
         String name = in.nextLine();
         System.out.println("Welcome to Java Programming World, " + name);
+        System.out.println("Good Luck," + name);
         in.close();
     }
 }
