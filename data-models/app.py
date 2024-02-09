558c014a30785ce6060b3dd3fed6ade76379ee03
Shazam007 <dananjayakavinduedu@gmail.com>
2024-02-09 15:31:21 +0200

    data-models added

---
+++ data-models/app.py
@@ -0,0 +1,93 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+from sqlalchemy import text
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_BASE_URI"] = "mysql+mysqldb://root@localhost/"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqldb://root@localhost/workout_playlists"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    height = db.Column(db.Float, nullable=False)
+    weight = db.Column(db.Float, nullable=False)
+    user_type = db.Column(db.String(32), nullable=False)
+    user_token = db.Column(db.String(64), nullable=False)
+    token_expiration = db.Column(db.DateTime, nullable=False)
+
+    workout_plan = db.relationship("WorkoutPlan", back_populates="user")
+
+class Workout(db.Model):
+    workout_id = db.Column(db.Integer, primary_key=True)
+    workout_name = db.Column(db.String(64), nullable=False)
+    duration = db.Column(db.Float, nullable=False)
+    workout_intensity = db.Column(db.String(32), nullable=False)
+    equipment = db.Column(db.String(64), nullable=False)
+    workout_type = db.Column(db.String(64), nullable=False)
+
+    workout_plan_item = db.relationship("WorkoutPlanItem", back_populates="workout")
+
+class WorkoutPlanItem(db.Model):
+    item_id = db.Column(db.Integer, primary_key=True)
+    workout_plan_id = db.Column(db.Integer, db.ForeignKey("workout_plan.workout_plan_id"))
+    workout_id = db.Column(db.Integer, db.ForeignKey("workout.workout_id"))
+
+    workout_plan = db.relationship("WorkoutPlan", back_populates="workout_plan_item")
+    workout = db.relationship("Workout", back_populates="workout_plan_item")
+
+class WorkoutPlan(db.Model):
+    workout_plan_id = db.Column(db.Integer, primary_key=True)
+    plan_name = db.Column(db.String(64), nullable=False)
+    duration = db.Column(db.Float, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    playlist_id = db.Column(db.Integer, db.ForeignKey("playlist.playlist_id"))
+
+    workout_plan_item = db.relationship("WorkoutPlanItem", back_populates="workout_plan")
+    user = db.relationship("User", back_populates="workout_plan")
+    playlist = db.relationship("Playlist", back_populates="workout_plan")
+
+class Playlist(db.Model):
+    playlist_id = db.Column(db.Integer, primary_key=True)
+    playlist_duration = db.Column(db.Float, nullable=False)
+
+    playlist_item = db.relationship("PlaylistItem", back_populates="playlist")
+
+class PlaylistItem(db.Model):
+    item_id = db.Column(db.Integer, primary_key=True)
+    song_id = db.Column(db.Integer, db.ForeignKey("song.song_id"))
+    playlist_id = db.Column(db.Integer, db.ForeignKey("playlist.playlist_id"))
+
+    song = db.relationship("Song", back_populates="playlist_item")
+    playlist = db.relationship("Playlist", back_populates="playlist_item")
+
+class Song(db.Model):
+    song_id = db.Column(db.Integer, primary_key=True)
+    song_name = db.Column(db.String(64), nullable=False)
+    song_artist = db.Column(db.String(64), nullable=False)
+    song_genre = db.Column(db.String(64), nullable=False)
+    song_duration = db.Column(db.Float, nullable=False)
+
+    playlist_item = db.relationship("PlaylistItem", back_populates="song")
+
+
+def create_database():
+    engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_BASE_URI'], echo=True)
+    conn = engine.connect()
+    try:
+        query = "CREATE DATABASE IF NOT EXISTS workout_playlists;"
+        conn.execute(text(query))
+        print("Database created successfully.")
+    except Exception as e:
+        print("Error creating database:", e)
+    conn.close()
+
+def create_tables():
+    db.metadata.create_all(bind=db.engine, checkfirst=True)
+
+with app.app_context():
+    create_database()
+    db.create_all()
