14ab9660a97c3054fa57081397af04a4821d63c8
Janne Kerola <janne.kerola@oulu.fi>
2024-03-02 18:30:26 +0200

    fix middleware

--- api/middleware/authguard.py
+++ api/middleware/authguard.py
@@ -1,12 +1,22 @@
+"Contains decorator functions related to authenticated routes"
 from functools import wraps
 from flask import request
-from api.services.jwt import JWTService
-from api.database import db
 from werkzeug.exceptions import Unauthorized
 from jwt.exceptions import DecodeError
+from api.services.jwt import JWTService
+from api.database import db
 
 
 def requires_authentication(f):
+    """Decorator function which protects routes from unauthorized access
+
+    usage:
+    @requires_authentication()
+    @route.get("/")
+    def my_route(user):
+        <do stuff with the authenticated user information>
+    """
+
     @wraps(f)
     def parse_authorization():
         auth_type = None
