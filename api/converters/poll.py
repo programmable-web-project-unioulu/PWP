b9ebfe0653b2054223943e0fae147def6db43163
Janne Kerola <janne.kerola@oulu.fi>
2024-03-02 18:31:24 +0200

    fix converters

--- api/converters/poll.py
+++ api/converters/poll.py
@@ -1,14 +1,18 @@
+"""Module containing the PollConverter"""
+
 from werkzeug.routing import BaseConverter
 from werkzeug.exceptions import NotFound
 from prisma.models import Poll
 
 
 class PollConverter(BaseConverter):
-    def to_python(self, poll_id):
-        poll = Poll.prisma().find_first(where={"id": poll_id})
+    """Converts the poll_id url-parameter into a Poll"""
+
+    def to_python(self, value):
+        poll = Poll.prisma().find_first(where={"id": value})
         if poll is None:
             raise NotFound
         return poll.id
 
-    def to_url(self, poll):
-        return poll.id
+    def to_url(self, value):
+        return value.id
